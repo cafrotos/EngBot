f47c51557707fab0fcb5964c598c70aa4dfd30da
minhphuong <nguyenminhphuong@unimarmores.com>
2018-10-18 00:54:17 +0700

    test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let Engbot = (req, res) => {
-  let message = req.query['mesage'];
-  res.send(message);
-}
+  let message = req.query.message || req.body.message || 'Hello World!';
+  res.status(200).send(message);
+};
+
 exports.EngbotFunction = Engbot;
